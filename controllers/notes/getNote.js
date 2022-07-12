76079270afc8655450038e9625c3e87bd3eea778
caroojeda2022 <102704165+caroojeda2022@users.noreply.github.com>
2022-07-12 20:12:45 +0200

    Update getNote.js

--- controllers/notes/getNote.js
+++ controllers/notes/getNote.js
@@ -1,1 +1,22 @@
+const selectNoteByIdQuery = require('../../db/noteQueries/selectNoteByIdQuery');
 
+const getNote = async (req, res, next) => {
+  try {
+    const { idNote } = req.params;
+
+    const { idUser } = req;
+
+    const note = await selectNoteByIdQuery(idUser, idNote);
+
+    res.send({
+      status: 'ok',
+      data: {
+        note,
+      },
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = getNote;
