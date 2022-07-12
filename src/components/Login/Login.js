cb270b8f6bce461e12e7b9700593cbdfef5d50fd
caroojeda2022 <102704165+caroojeda2022@users.noreply.github.com>
2022-07-12 21:36:48 +0200

    Update Login.js

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,95 +1,89 @@
 import { useState } from 'react';
-import { Navigate } from 'react-router-dom';
-import { useMessage } from '../../MessageContext';
 import { useToken } from '../../TokenContext';
-import { Toaster } from 'react-hot-toast';
-import useToast from '../../hooks/useToast';
-
 import './Login.css';
 
 const Login = () => {
-    const [token, setToken] = useToken();
-    const [, setMessage] = useMessage();
+  //hook personalizado
+  const [token, setToken] = useToken();
 
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [loading, setLoading] = useState(false);
+  //Variable del state
+  const [loginuser, setloginUser] = useState('');
+  const [registeruser, setregisterUser] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
 
-    // Si hay alguna nota en el contexto lo imprimimos con toast.
-    // Para ello utilizaremos este hook personalizado.
-    useToast();
+  //si estamos logeados, existe token, se va a la home
+if (token) return <Navigate to='/' />;  
 
-    // Si estamos logueados redireccionamos a la página principal.
-    if (token) return <Navigate to='/' />;
+  //Función de envio del manejo del formulario
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-        setLoading(true);
+    setLoading(true);
 
-        try {
-            const res = await fetch('http://localhost:4000/login', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    email,
-                    password,
-                }),
-            });
+    try {
+      const res = await fetch('http://localhost:3000/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email,
+          password,
+        }),
+      });
 
-            const body = await res.json();
+      const body = await res.json();
 
-            if (body.status === 'error') {
-                setMessage({
-                    status: 'error',
-                    text: body.message,
-                });
-            } else {
-                setToken(body.data.token);
-            }
-        } catch (err) {
-            setMessage({
-                status: 'error',
-                text: err.message,
-            });
-        } finally {
-            setLoading(false);
-        }
-    };
+      if (body.status === 'error') {
+        //muestra un mensaje de error con 1 alerta
+        window.alert(body.message);
+      } else {
+        //muestra un mensaje de error con 1 alerta
+        window.alert('Login correcto');
+        //Actualiza el token
+        setToken(body.data.token);
+      }
+    } catch (err) {
+      console.error(err);
+    } finally {
+      setLoading(false);
+    }
+  };
 
-    return (
-        <main className='login'>
-            <form onSubmit={handleSubmit}>
-                <label htmlFor='email'>Email:</label>
-                <input
-                    type='email'
-                    name='email'
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                />
+  return (
+    <main className='login'>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor='email'>Email:</label>
+        <input
+          type='email'
+          name='email'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
 
-                <label htmlFor='pass'>Contraseña:</label>
-                <input
-                    type='password'
-                    name='password'
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                />
+        <label htmlFor='pass'>Contraseña:</label>
+        <input
+          type='password'
+          name='pass'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
 
-                <button disabled={loading}>Log In</button>
-            </form>
-            <Toaster
-                containerStyle={{
-                    position: 'relative',
-                    width: '100%',
-                    left: '50%',
-                    transform: 'translateX(-50%)',
-                }}
-            />
-        </main>
-    );
+        <button disabled={loading}>Log In</button>
+      </form>
+      <Toaster
+        containerStyle={{
+          position: 'relative',
+          width: '100%',
+          left: '50%',
+          transform: 'translateX(-50%)',
+        }}
+      />
+    </main>
+  );
 };
 
 export default Login;
