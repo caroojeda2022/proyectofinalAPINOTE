892308eeaa408030cc9592d3234120c52b8cbe6d
caroojeda2022 <102704165+caroojeda2022@users.noreply.github.com>
2022-07-12 20:19:35 +0200

    Create index.js

---
+++ controllers/user/index.js
@@ -0,0 +1,11 @@
+const newUser = require('./newUser');
+const getUser = require('./getUser');
+const getOwnUser = require('./getOwnUser');
+const loginUser = require('./loginUser');
+
+module.exports = {
+  newUser,
+  getUser,
+  getOwnUser,
+  loginUser,
+};
